68b9fff44afe7fd9edf23390b51c7ed19a04db0e
David Anglada <danglada@perception.es>
2020-12-16 17:37:29 +0100

    Add i18n with ca and es translations

---
+++ src/i18n/ca.js
@@ -0,0 +1,30 @@
+
+// Example config that shows how to overwrite translations:
+// https://github.com/kiprotect/klaro/blob/master/src/translations/ca.yml
+klaroConfig.translations.ca = {
+    consentModal: {
+        title: 'Cookies que s\'utilitzen en aquesta web',
+        description: 'Aquí podeu veure i personalitzar les cookies que utilitzem en aquest web.',
+    },
+    privacyPolicy: {
+        text: 'Per saber més sobre la nostra política de cookies, visiteu aquest enllaç: {privacyPolicy}.',
+        name: 'Política de cookies'
+    },
+    consentNotice: {
+        description: 'Aquesta pàgina web utilitza cookies. Les cookies d\'aquest lloc web s\'utilitzen per diferents motius: {purposes}.',
+        changeDescription: 'Hi ha hagut canvis des de la seva última visita, si us plau, actualitzi el seu consentiment.',
+        learnMore: 'Configurar',
+    },
+    ok: 'Acceptar',
+    acceptAll: 'Acceptar totes',
+    acceptSelected: 'Acceptar seleccionades',
+    purposes: {
+        necessary: 'Necessària',
+        analytics: 'Analítica',
+        statistics: 'Estadística',
+        security: 'Seguretat',
+        livechat: 'Livechat',
+        advertising: 'Publicitat',
+        styling: 'Estils'
+    }
+};
---
+++ src/i18n/es.js
@@ -0,0 +1,30 @@
+
+// Example config that shows how to overwrite translations:
+// https://github.com/kiprotect/klaro/blob/master/src/translations/es.yml
+klaroConfig.translations.es = {
+    consentModal: {
+        title: 'Cookies que se utilizan en esta web',
+        description: 'Aquí puede ver y personalizar las cookies que utilizamos en esta web.'
+    },
+    privacyPolicy: {
+        text: 'Para saber más sobre nuestra política de cookies, visite este enlace: {privacyPolicy}.',
+        name: 'Política de cookies'
+    },
+    consentNotice: {
+        description: 'Esta página web utiliza cookies. Las cookies de este sitio web se utilizan con las siguientes finalidades: {purposes}.',
+        changeDescription: 'Ha habido cambios desde su última visita, por favor, actualice su consentimiento.',
+        learnMore: 'Configurar',
+    },
+    ok: 'Aceptar',
+    acceptAll: 'Aceptar todas',
+    acceptSelected: 'Aceptar seleccionadas',
+    purposes: {
+        necessary: 'Necesaria',
+        analytics: 'Analítica',
+        statistics: 'Estadística',
+        security: 'Seguridad',
+        livechat: 'Livechat',
+        advertising: 'Publicidad',
+        styling: 'Estilos'
+    }
+};
